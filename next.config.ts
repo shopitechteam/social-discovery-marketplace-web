a07d31e2bb3d1fe297aec482acab65daf94d4cee
emmamwas99 <emmamwas99@gmail.com>
2026-07-15 22:54:31 +0300

    hiden tiktok and apple login

--- next.config.ts
+++ next.config.ts
@@ -205,6 +205,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.shopi.co.ke",
+      },
     ],
   },
 };
